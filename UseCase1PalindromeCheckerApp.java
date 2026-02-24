af220f732921804df336408c436071a93a794772
KetkiGonnade <ketkigonnade@gmail.com>
2026-02-24 09:48:36 +0000

    Usecase1 completed

---
+++ UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,16 @@
+
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        displayWelcomeMessage();
+    }
+    private  static void displayWelcomeMessage() {
+
+        System.out.println("   WELCOME TO PALINDROME CHECKER APP   ");
+        System.out.println("Application Version : 1.0");
+        System.out.println("System Installation successful.");
+   
+
+    }
+}
